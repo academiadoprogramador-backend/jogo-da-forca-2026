2d94ebe0404330cbb4a0522b213ef168b79202ff
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-03-26 19:49:17 -0300

    Adicionada estrutura do projeto

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Requisitos
+        1. Ao iniciar o jogo, deve ser selecionada uma palavra aleatória à partir de uma lista.
+        2. O jogador poderá chutar a palavra secreta letra por letra, cada letra certa deverá ser apresentada,
+        assim como as letras erradas.
+        3. O jogador poderá cometer até cinco erros, caso erre pela quinta vez, ou acerte a palavra a partida
+        acaba.
+        4. Deve-se apresentar um desenho da forca sendo atualizado a cada erro.
+*/
+
+
+// 1. Ao iniciar o jogo, deve ser selecionada uma palavra aleatória à partir de uma lista.
+string palavraSecreta = "ABACATE";
+
+// 2. O jogador poderá chutar a palavra secreta letra por letra, cada letra certa
+// deverá ser apresentada.
+bool jogadorAcertou = false;
+
+while (!jogadorAcertou)
+{
+    Console.Clear();
+    Console.Write("Digite uma letra: ");
+    char letra = Convert.ToChar(Console.ReadLine());
+
+    Console.WriteLine(letra);
+    Console.ReadLine();
+}
